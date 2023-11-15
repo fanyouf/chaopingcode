d64e11bc41a1f5ed1f7a922a499ca958c6ec7fdb
Your Name <you@example.com>
2023-11-15 15:11:50 +0800

    fixed: 指令库bugs

--- src/api/directive.ts
+++ src/api/directive.ts
@@ -10,7 +10,7 @@ export function getList(params: any) {
   }>({
     url: 'http://8.142.32.7:8888/directive',
     method: 'get',
-    params,
+    params: { sortField: 'order', sortOrder: 'asc', ...params },
   })
 }
 
--- src/api/directiveGroup.ts
+++ src/api/directiveGroup.ts
@@ -12,7 +12,7 @@ export function getList(params: any) {
   }>({
     url: 'http://8.142.32.7:8888/directiveGroup',
     method: 'get',
-    params,
+    params: { sortField: 'order', sortOrder: 'asc', ...params },
   })
 }
 
